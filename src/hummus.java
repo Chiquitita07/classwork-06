10c45981e18df11741618e8a2f72f194fc9a3a24
Chiquitita07 <benari.noga@gmail.com>
2022-12-14 18:55:03 +0200

    "scanner Scanner"

--- src/hummus.java
+++ src/hummus.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class hummus {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         int min = Integer.MAX_VALUE, max = 0, price;
-        price = s.nextInt();
+        price = scanner.nextInt();
         while (price > 0) {
             System.out.println("Enter the hummus prices of different restaurants: ");
-            price = s.nextInt();
+            price = scanner.nextInt();
             if (price > max) {
                 max = price;
             }
